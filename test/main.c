3295ee29924e11ec42691982785379a38473687f
乔晓波 <1587510366@qq.com>
2017-06-16 18:07:04 +0800

    ces
    c

--- test/main.c
+++ test/main.c
@@ -12,19 +12,21 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     //printf("Hello, World!\n");
     //printf("今天高考\n");
+    //  //Users//s20161104611//Desktop//test1//maopao.txt
     int a[20];
     int n,k,j,t,i;
-    FILE *fp1,*fp2;
-    fp1=fopen("//Users//s20161104611//Desktop//test1//sortout.txt","w");
-    fp2=fopen("//Users//s20161104611//Desktop//test1//sort.txt","r+");
+    //FILE *fp1,*fp2;
+    FILE *fp2;
+    //fp1=fopen("//Users//s20161104611//Desktop//test1//sortout.txt","w+");
+    fp2=fopen("//Users//s20161104611//Desktop//test1//GPS170510.log","r+");
     if(fp2==NULL)
     {
         printf("打开文件错误，要打开的文件可能不存在");
-        
     }
     else
-    {
-        while(fscanf(fp2,"%d",&n)!=EOF)
+    {//$GPRMC,013238,A,4047.533,N,11141.856,E,012.2,253.6,100517,,*1A
+        fscanf("$GPRMC,%s,%c,%ld,%c,%ld,%c,%ld,%ld,%s,,%s",time,gps,&latitude,latitude1,&longitude,longitude1,&v,&course,year,s)
+        /*while(fscanf(fp2,"%d",&n)!=EOF)
         {
             for(i=0;i<n;i++)
             {
@@ -50,7 +52,7 @@ int main(int argc, const char * argv[]) {
             fprintf(fp1,"\n");
             printf("\n");
             
-        }
+        }*/
         fclose(fp1);
         fclose(fp2);
     }
